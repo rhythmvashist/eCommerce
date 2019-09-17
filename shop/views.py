af758a488e470cb4bb29be1b030c9c8358f1528c
Rhythmvashist <vashistrhythm1@gmail.com>
2019-09-16 23:19:06 -0700

    working on temmplates and static files

--- shop/views.py
+++ shop/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from. models import  Product
+from math import ceil
 
 # Create your views here.
 
 def index(request):
-    return  render(request,"shop/index.html")
+    products=Product.objects.all()
+    print(products)
+    n=len(products)
+    nSlides=n//4+ceil((n/4)-(n//4))
+    params={
+        'product':products,
+        'no_of_slides':nSlides,
+        'range':range(nSlides)
+    }
+
+    return  render(request,"shop/index.html",params)
 
 def about(request):
     return  render(request,"shop/about.html")
