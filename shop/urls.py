782405716dceafaf9e495ca713f4e1c9f7f1472b
Rhythmvashist <vashistrhythm1@gmail.com>
2019-09-21 23:23:46 -0700

    fixed the carousel scrolling issue

--- shop/urls.py
+++ shop/urls.py
@@ -1,14 +1,12 @@
 from django.urls import path
 from . import views
 
-
 urlpatterns = [
     path('', views.index, name="shophome"),
-    path('about',views.about,name=' Aboutus'),
-    path('contact',views.contact,name=' Contactus'),
-    path('tracker',views.tracker,name=' TrackingStatus'),
-    path('search',views.search,name='Search'),
-    path('productview',views.productView,name='Productview'),
-    path('checkout',views.checkout,name='Checkout'),
-
+    path('about', views.about, name=' Aboutus'),
+    path('contact', views.contact, name=' Contactus'),
+    path('tracker', views.tracker, name=' TrackingStatus'),
+    path('search', views.search, name='Search'),
+    path('productview', views.productView, name='Productview'),
+    path('checkout', views.checkout, name='Checkout'),
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,38 +1,43 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from. models import  Product
+from .models import Product
 from math import ceil
+
 
 # Create your views here.
 
 def index(request):
-    products=Product.objects.all()
+    products = Product.objects.all()
     print(products)
-    n=len(products)
-    nSlides=n//4+ceil((n/4)-(n//4))
-    params={
-        'product':products,
-        'no_of_slides':nSlides,
-        'range':range(1,nSlides)
+    n = len(products)
+    nSlides = n // 4 + ceil((n / 4) - (n // 4))
+    params = {
+        'product': products,
+        'no_of_slides': nSlides,
+        'range': range(1, nSlides)
     }
-    return  render(request,"shop/index.html",params)
+    return render(request, "shop/index.html", params)
+
 
 def about(request):
-    return  render(request,"shop/about.html")
+    return render(request, "shop/about.html")
+
 
 def contact(request):
     return HttpResponse('we are at contact')
 
+
 def tracker(request):
     return HttpResponse('we are at tracker')
+
 
 def search(request):
     return HttpResponse('we are at search')
 
+
 def productView(request):
     return HttpResponse('we are at productView')
 
+
 def checkout(request):
     return HttpResponse('we are at checkout')
-
-
