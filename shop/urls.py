f7e8c266a1bc8da45b224e45a4e47d6cd5e13878
Rhythmvashist <vashistrhythm1@gmail.com>
2019-09-29 20:10:15 -0700

    added product view page

--- shop/urls.py
+++ shop/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('contact', views.contact, name=' Contactus'),
     path('tracker', views.tracker, name=' TrackingStatus'),
     path('search', views.search, name='Search'),
-    path('productview', views.productView, name='Productview'),
+    path('products/<int:myid>', views.productView, name='Productview'),
     path('checkout', views.checkout, name='Checkout'),
 ]
--- shop/views.py
+++ shop/views.py
@@ -3,30 +3,21 @@ from django.http import HttpResponse
 from .models import Product
 from math import ceil
 
-
 # Create your views here.
 
+
 def index(request):
-
-
-    # params = {
-    #     'product': products,
-    #     'no_of_slides': nSlides,
-    #     'range': range(1, nSlides)
-    # }
-    # list of list
     allprods = []
     categoriesList = Product.objects.values('category', 'id')
     print(categoriesList)
+
     categoryLt = {item['category'] for item in categoriesList}
     for category in categoryLt:
         prods = Product.objects.filter(category=category)
         print(prods)
         n = len(prods)
         nSlides = n//4 + ceil((n/4)-(n//4))
-        allprods.append([prods, range(1,nSlides), nSlides])
-
-
+        allprods.append([prods, range(1, nSlides), nSlides])
 
     params = {
         'allprods': allprods
@@ -39,20 +30,25 @@ def about(request):
 
 
 def contact(request):
-    return HttpResponse('we are at contact')
+    return render(request, "shop/contact.html")
 
 
 def tracker(request):
-    return HttpResponse('we are at tracker')
+    return render(request, "shop/tracker.html")
 
 
 def search(request):
-    return HttpResponse('we are at search')
+    return render(request, "shop/search.html")
 
 
-def productView(request):
-    return HttpResponse('we are at productView')
+def productView(request,myid):
+    #fetch the producct from db
+
+    product=Product.objects.filter(id=myid)
+    print(product)
+
+    return render(request, "shop/prodView.html", {'product': product[0]})
 
 
 def checkout(request):
-    return HttpResponse('we are at checkout')
+    return render(request, "shop/checkout.html")
